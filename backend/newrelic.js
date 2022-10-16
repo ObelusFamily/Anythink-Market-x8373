c23e97042566bc7c1cb440a4ae6151bbf26a9fff
Ben Konicek <ben.konicek@gmail.com>
2022-10-15 21:51:00 -0400

    remove license key

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: 'ee9117a5be64baaf2a8aa5b4b37a9b590113NRAL',
+  license_key: '',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
